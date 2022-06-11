74dc46fc7675c6311ff1642b25bababa12435731
Ivan Franchin <ivan.franchin@takeaway.com>
2022-06-11 16:23:54 +0200

    uncomment SpringBootTest annotation from test classes

--- avro-3-de-serialization/avro-3-consumer-service/src/test/java/com/mycompany/avro3consumerservice/Avro3ConsumerServiceApplicationTests.java
+++ avro-3-de-serialization/avro-3-consumer-service/src/test/java/com/mycompany/avro3consumerservice/Avro3ConsumerServiceApplicationTests.java
@@ -2,10 +2,10 @@ package com.mycompany.avro3consumerservice;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest;
 
 @Disabled
-//@SpringBootTest // commented this annotation because it's failing to build the native image
+@SpringBootTest
 class Avro3ConsumerServiceApplicationTests {
 
     @Test
--- avro-de-serialization/avro-producer-service/src/test/java/com/mycompany/avroproducerservice/AvroProducerServiceApplicationTests.java
+++ avro-de-serialization/avro-producer-service/src/test/java/com/mycompany/avroproducerservice/AvroProducerServiceApplicationTests.java
@@ -2,10 +2,10 @@ package com.mycompany.avroproducerservice;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest;
 
 @Disabled
-//@SpringBootTest // commented this annotation because it's failing to build the native image
+@SpringBootTest
 class AvroProducerServiceApplicationTests {
 
     @Test
